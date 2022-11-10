99d6d2ad4a5a6f10ddd4d71e9915c04d2bfc67f8
Raph <91033429+raphaelpech@users.noreply.github.com>
2022-11-10 17:42:23 +0100

    Modif Traitre

--- src/personnages/secondaires/Traitre.java
+++ src/personnages/secondaires/Traitre.java
@@ -1,5 +1,12 @@
 package personnages.secondaires;
 
-public class Traitre {
+import personnages.principaux.Samourai;
+
+public class Traitre extends Samourai {
+
+	public Traitre(String nom, String boisson, int argent, String seigneur) {
+		super(nom, boisson, argent, seigneur);
+		// TODO Auto-generated constructor stub
+	}
 
 }
